70bfc2331ad1235e942a7393dba1f23da932057b
Lucas Costa <lucas.co090869@gmail.com>
2024-07-13 19:18:41 -0300

    chore: add links retrieving route get-links

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -28,6 +29,7 @@ app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivities);
 app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running on http://localhost:3333");
