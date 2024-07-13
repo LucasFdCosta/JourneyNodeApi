814697c333a73d23698db6dfb7a9dcbcce185a1b
Lucas Costa <lucas.co090869@gmail.com>
2024-07-13 20:38:55 -0300

    feat: create invite creation route
    create a route to create an invite to a participant

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # The NodeJS version of [Journey](https://github.com/LucasFdCosta/Journey/tree/master/api) application's API
 I created this project to make a NodeJS version of my other application's API that I created using .NET
+
+## Prerequisites
+### Create a .env file with the environment variable containing the path to your SQLite DB file 
+
+```
+DATABASE_URL="file:{YourPathToDbFileGoesHere}"
+```
+
+### Install dependencies
+```
+npm install
+```
+
+### Run application in dev mode
+```
+npm run dev
+```
---
+++ src/routes/create-invite.ts
@@ -0,0 +1,77 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
+
+/**
+ * Creates an invite for a trip.
+ *
+ * @param {FastifyInstance} app - The Fastify instance.
+ * @return {Promise<{ participantId: string }>} - A promise that resolves to an object with the ID of the created participant.
+ */
+export async function createInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post("/trips/:tripId/invites", {
+    schema: {
+      params: z.object({
+        tripId: z.string().uuid(),
+      }),
+      body: z.object({
+        email: z.string().email(),
+      })
+    },
+  }, async (request) => {
+    const { tripId } = request.params;
+    const { email } = request.body;
+
+    const trip = await prisma.trip.findUnique({
+      where: {
+        id: tripId
+      }
+    });
+
+    if (!trip) {
+      throw new Error("Trip not found");
+    }
+
+    const participant = await prisma.participant.create({
+      data: {
+        email,
+        trip_id: tripId
+      }
+    })
+
+    const formattedStartDate = dayjs(trip.starts_at).format("LL");
+    const formattedEndDate = dayjs(trip.ends_at).format("LL");
+
+    const mail = await getMailClient();
+
+    const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`;
+
+    const message = await mail.sendMail({
+      from: {
+        name: "Plann.er Team",
+        address: "hi@planner.com"
+      },
+      to: participant.email,
+      subject: `Confirm your presence in the trip to ${trip.destination} on ${formattedStartDate}`,
+      html: `
+        <div style="font-family: sans-serif font-size: 16px line-height: 1.6;">
+          <p>You have been invited to a trip to <strong>${trip.destination}</strong> from <strong>${formattedStartDate}</strong> to <strong>${formattedEndDate}</strong></p>
+          <p></p>
+          <p>We are waiting for your confirmation to join the trip to ${trip.destination}. Please confirm your participation at your earliest convenience.</p>
+          <p></p>
+          <a href="${confirmationLink}">Confirm trip</a>
+          <p></p>
+          <p>In case you did not create this trip, please ignore this email.</p>
+        </div>
+        `.trim(),
+    })
+
+    console.log(nodemailer.getTestMessageUrl(message));
+
+    return { participantId: participant.id };
+  })
+}
