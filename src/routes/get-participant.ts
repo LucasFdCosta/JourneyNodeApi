7165c7c3023d2d1d7b8c766dd63d3c63781c98d9
Lucas Costa <lucas.co090869@gmail.com>
2024-07-13 21:06:41 -0300

    feat: create participant retrieving route

---
+++ src/routes/get-participant.ts
@@ -0,0 +1,41 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { prisma } from "../lib/prisma";
+
+/**
+ * Retrieves a participant by their ID.
+ *
+ * @param {FastifyInstance} app - The Fastify instance.
+ * @return {Promise<{ participant: { id: string, name: string, email: string, is_confirmed: boolean } }>} - A promise that resolves to an object containing the participant's ID, name, email, and confirmation status.
+ * @throws {Error} - If the participant is not found.
+ */
+export async function getParticipant(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get("/participants/:participantId", {
+    schema: {
+      params: z.object({
+        participantId: z.string().uuid(),
+      })
+    },
+  }, async (request) => {
+    const { participantId } = request.params;
+
+    const participant = await prisma.participant.findUnique({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        is_confirmed: true
+      },
+      where: {
+        id: participantId
+      }
+    });
+
+    if (!participant) {
+      throw new Error("Participant not found");
+    }
+
+    return { participant };
+  })
+}
